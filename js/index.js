f5dbd99f87582fb651cba7b8cf36ceb7a479e7f4
Kaylee Knox <kjknoxx90@gmail.com>
2023-09-22 17:09:01 -0500

    Pushing before I stop for a while.

--- js/index.js
+++ js/index.js
@@ -8,27 +8,35 @@ import {movieKey} from "./keys.js";
 // 3. Make our main function call necessary parts
 // 4. CSS/Style our site more
 
+//This function makes the cards already in our DB, still need a function to call our cards, this will go into the async section
+const renderMovie = (movies, favorite) => {
+    const modal = document.createElement("div");
+    model.classList.add('modal');
+    model.innerHTML = `            <article class="fav-card">
+                <h3 class="movie-title">${movies[0].title}</h3>
+                <p class="movie-year">Year: ${movies[0].release_date}</p>
+                <p class="movie-description">${movies[0].overview}</p>
+                <div class="d-flex align-items-center justify-content-between">
+                    <span>My Rating:</span>
+                    <span class="personal-card-rating"></span>
+                </div>
+                <span>Average Rating:</span>
+                <meter class="movie-meter" min="0" max="10" value="4"></meter>
+                <div class="d-flex, align-items-center justify-content-start">
+                ${renderGenres(movies[0].genre_ids[0])}
+                </div>
+            </article>
+        ` }
+
+const renderGenres = (genre) => {
+    const genreHTML = genre.map((genre) => `<span class="movie-card-tag">${movies[0].genre_id[0]}</span>`).join("");
+    return genreHTML;
+};
+
 (async() => {
-
-    // const  getMoviesAPI("Shrek");
-    // await getMoviesDB("Shrek");
-    // await getMoviesAPI('jaws');
-    const renderModel = (movies, favorite) => {
-        const modal = document.createElement("div");
-        model.classList.add('modal');
-        model.innerHTML = `
-            <div class="fav-card">
-                <h3>Movie Title</h3>
-                <img src="" alt="movie icon">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut sem viverra aliquet eget.</p>
-                <a href="#">Genre 1</a> <a href="#">Genre 2</a> <a href="#">Genre 3</a>
-                My Rating
-                <meter class="book-card-meter" min="0" max="10" value="4"></meter>
-
-                Official Rating
-                <meter class="book-card-meter" min="0" max="10" value="4"></meter>
-
-                <button type="button">Remove</button>
-            </div>`
+    const movies = await getMoviesDB();
+    for (let movie of movies) {
+        const target = document.querySelector(".movie-grid");
+        renderMovie(movie, target);
     }
 })();
