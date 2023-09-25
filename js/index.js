1fef9906d731a316fe93ce2da86c03abade058ca
Kaylee Knox <kjknoxx90@gmail.com>
2023-09-25 11:29:13 -0500

    Working on add, delete, and edit buttons

--- js/index.js
+++ js/index.js
@@ -1,101 +1,86 @@
-import {getMoviesAPI, getMoviesDB, addMovie, deleteMovie, patchMovie} from "./movie-functs/movie-functions.js"
+import {getMovieSearch, getMoviesDB, addMovie, deleteMovie, patchMovie, getTop20} from "./movie-functs/movie-functions.js"
 import {movieKey} from "./keys.js";
 
 //TODO: Functions are done, so now we have to:
 
-// 1. Must make our const to produce cards that are shown in our favorites
-// 2. Must make our const that will show randomly generated cards in our other section
-// 3. Make our main function call necessary parts
-// 4. CSS/Style our site more
+// 1. Have to add: Add button, Delete button, and edit button
+// 2. search function to search through movies
+// 3. Make website look nice with CSS
 
-//This function makes the cards already in our DB, still need a function to call our cards, this will go into the async section
-const renderMovie = (movies, favorite) => {
+//This is a function that will render the top 20 cards
+const renderTop20 = (movies, type) => {
     const modal = document.createElement("div");
     modal.classList.add('modal');
-    modal.innerHTML = `            <article class="fav-card">
-                <h3 class="movie-title">${movies[0].title}</h3>
-                <p class="movie-year">Year: ${movies[0].release_date}</p>
-                <p class="movie-description">${movies[0].overview}</p>
+    modal.innerHTML = `            <article class="${type}">
+                <h3 class="movie-title">${movies.title}</h3>
+                <p class="movie-year">Year: ${movies.release_date}</p>
+                <p class="movie-description">${movies.overview}</p>
+                <span>Average Rating:</span>
+                <meter class="movie-meter" min="0" max="10" value="${movies.vote_average}"></meter><br>
+                <button class="add-movie-button">Add Movie</button>
+            </article>
+        `
+    //Calling the cards specifically to the div we want in it.
+    const movieGrid = document.getElementById('movie-grid')
+    movieGrid.appendChild(modal)
+};
+
+// This is a function that will render the favorites we've added to movies.json
+const renderFavs = (movies, type) => {
+    const modal = document.createElement("div");
+    modal.classList.add('modal');
+    modal.innerHTML = `            <article class="${type}">
+                <h3 class="movie-title">${movies.title}</h3>
+                <p class="movie-year">Year: ${movies.release_date}</p>
+                <p class="movie-description">${movies.overview}</p>
                 <div class="d-flex align-items-center justify-content-between">
                     <span>My Rating:</span>
                     <span class="personal-card-rating"></span>
                 </div>
                 <span>Average Rating:</span>
-                <meter class="movie-meter" min="0" max="10" value="4"></meter>
-                <div class="d-flex, align-items-center justify-content-start">
-                ${renderGenres(movies[0].genre_ids[0])}
-                </div>
+                <meter class="movie-meter" min="0" max="10" value="${movies.vote_average}"></meter><br>
+                <button class="delete-button">Delete</button>
             </article>
-        ` }
-
-const renderGenres = (genre) => {
-    const genreHTML = genre.map((genre) => `<span class="movie-card-tag">${movies[0].genre_id[0]}</span>`).join("");
-    return genreHTML;
+        `
+    //Calling the cards specifically to the div we want in it.
+    const favGrid = document.getElementById('fav-grid')
+    favGrid.appendChild(modal)
 };
 
+const grabId = () =>{
+
+}
+
+
+const deleteButton = document.getElementsByClassName('delete-button');
+deleteButton.addEventListener ("Click", ()=>{
+
+})
+
+//Loader
+document.documentElement.onload =function (){
+    document.getElementById('loader').style.display ='block';
+};
+window.onload= function (){
+    document.getElementById('loader').style.display='none';
+};
+
+//MAIN FUNCTION//
 (async() => {
-    const movies = await getMoviesDB();
-    for (let movie of movies) {
-        const target = document.querySelector(".movie-grid");
-        renderMovie(movie, target);
-    }
+    // This loop calls our card render function **for each** one that is listed as Top 20
+    const movies = await getTop20();
+    movies.results.forEach(movie => {
+        const target = document.getElementById("movie-grid")
+        renderTop20(movie, "fav-card");
+    })
+
+    // This loop calls our card render function for our database movies ***for each*** one that is in our database.
+    const faves = await getMoviesDB();
+    console.log(faves.results);
+    faves.forEach(movie => {
+        const target = document.getElementById('fav-grid')
+        renderFavs(movie, "fav-card");
+    })
+
+
 })();
-
-
-
-
-
-//
-// const api_key = 'api_key=48f68dcd82cc8a35116e0848fdceaeb6';
-// const base_url = 'https://api.themoviedb.org/3';
-// const api_url = base_url + '/discover/movie?sort_by=popularity.desc&' + api_key;
-// const img_url = 'https://image.tmbd.org/t/p/500';
-//
-// const main = document.getElementById('main');
-//
-// getMovies(api_url);
-// async function getMovies(url) {
-//     fetch(url).then(res => res.json()).then(data => {
-//         // console.log(data);
-//         showMovies(data);
-//     })
-// }
-// let data1 = getMoviesAPI().value
-// function showMovies(data) {
-//     main.innerHTML = "";
-//
-//     data.foreach(movie => {
-//         const {title, poster_path, vote_average, overview, release_date}= movie;
-//         const movieEl = document.createElement('div');
-//         movie.classlist.add('movie');
-//         movieEl.innerHTML = `
-//        <h3 class="movie-title">${title}}</h3>
-//        <img src="${img_url + poster_path}" alt="${title}}
-//         <p class="movie-year">${release_date}</p>
-//         <p class="movie-description">${overview}</p>
-//         <div class="d-flex align-items-center justify-content-between">
-//         <span>My Rating:</span>
-//         <span class="personal-card-rating ${getColor(vote_average)}">${vote_average}</span>
-//         </div>
-//         <meter class="movie-meter" min="0" max="10" value="4"></meter>
-//         <div class="d-flex, align-items-center justify-content-start">
-//         <div class="movie-genre-tag">${genre_ids}</div>
-//         </div>
-//
-//         `
-//
-//         console.log(movieEl);
-//         main.appendChild(movieEl);
-//
-//     })
-// }
-//
-// function getColor(vote) {
-//     if(vote >= 8) {
-//         return 'green';
-//     } else if (vote >= 5) {
-//         return 'orange';
-//     } else {
-//         return 'red';
-//     }
-// }
--- js/movie-functs/movie-functions.js
+++ js/movie-functs/movie-functions.js
@@ -1,7 +1,7 @@
 import {movieKey} from "../keys.js";
 
 // Function to search the API database for a movie based on title
-const getMoviesAPI = async (query) => {
+const getMovieSearch = async (query) => {
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${movieKey}&language=en-US&query=${query}&page=1&include_adult=false`;
     const options = {
         method: "GET",
@@ -16,8 +16,8 @@ const getMoviesAPI = async (query) => {
 };
 
 // Function to search OUR Database for movies added to our favorites
-const getMoviesDB = async (title) => {
-    const url = `http://localhost:3000/movies?title=${title}`;
+const getMoviesDB = async () => {
+    const url = `http://localhost:3000/movies`;
     const options = {
         method: "GET",
         headers: {
@@ -26,7 +26,6 @@ const getMoviesDB = async (title) => {
     };
     const res = await fetch(url, options);
     const movies = await res.json();
-    console.log(movies);
     return movies;
 };
 
@@ -89,172 +88,20 @@ const patchMovie = async (movie) => {
     }
 };
 
-export {getMoviesAPI, getMoviesDB, addMovie, deleteMovie, patchMovie}
+// Function  to call top 20 from API
+const getTop20 = async () => {
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZDBhZWE4NDZlM2Q1ZmJmZjRhZmUxMjBiZDYxNTVhNiIsInN1YiI6IjY1MGM1Y2QwYjViYzIxMDBlYWNhYmNjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aWmrat2iN2I9euQNdvYrPsJk4k9X8FCf1LORWOey1Ow'
+        }
+    };
+    const res = await fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+    const movies =  await res.json();
+    return movies;
+};
 
 
+export {getMovieSearch, getMoviesDB, addMovie, deleteMovie, patchMovie, getTop20}
 
-//
-//
-//
-//
-//
-//
-//
-//What I got to use or reference as a lifeline from someone.. but without being able to ask questions I am still fuzzy
-
-'use strict';
-// Code executed after HTML is full loaded.
-document.addEventListener('DOMContentLoaded', function () {
-    let titleArr = [];
-    let ratingArr = [];
-    let genreArr = [];
-    let movieCount = 0;
-    // Function updates the total movie count displayed in the HTML.
-    function updateMovieCount() {
-        document.getElementById('movieCount').innerHTML = `Total Movies: ${movieCount}`;
-    }
-    // Function used to update the total movie count displayed.
-    // Async function fetches movie data from server and generates HTML element for each movie and updates the empty arrays and movieCount.
-    async function fetchMovies() {
-        try {
-            const response = await fetch('http://localhost:3000/movies');
-            const data = await response.json();
-            data.forEach(function (element) {
-                const movieCard = `
-                <div id="${element.id}" class="movieCard">
-                <h2 class="movieTitle">${element.title}</h2>
-                <p class="movieGenre">${element.genre}</p>
-                <p class="movieRating">${element.rating} <i class="fa-solid fa-star fa-2xl" style="color: #004C5B;"></i></p>
-                <button class="editButton">Edit</button>
-                <button class="deleteButton">Delete</button>
-                </div>`;
-                document.getElementById('movies').insertAdjacentHTML('beforeend', movieCard);
-                titleArr.push(element.title);
-                ratingArr.push(element.rating);
-                genreArr.push(element.genre);
-            });
-            movieCount = data.length;
-            updateMovieCount();
-            document.getElementById('loading').style.display = 'none';
-            document.getElementById('addMovieContainer').style.display = 'block';
-        } catch (error) {
-            console.error(error);
-        }
-    }
-    // Function retrieves movie data from form inputs and return as an object
-    function addMovie() {
-        const title = document.getElementById('movieTitle').value;
-        const rating = document.getElementById('movieRating').value;
-        const genre = document.getElementById('movieGenre').value;
-        return { title, rating, genre };
-    }
-    // Function clears the input fields in the "Add Movie" form.
-    function resetAddMovieForm() {
-        document.getElementById('movieTitle').value = '';
-        document.getElementById('movieRating').value = '';
-        document.getElementById('movieGenre').value = '';
-    }
-    // Function to display modal for editing movie details when the "Edit" button is clicked and updates the modal with existing movie data
-    function displayEditModal(movieCard) {
-        const movieId = parseInt(movieCard.id);
-        const title = movieCard.querySelector('.movieTitle').textContent;
-        const rating = movieCard.querySelector('.movieRating').textContent;
-        const genre = movieCard.querySelector('.movieGenre').textContent;
-        const editModal = document.getElementById('editModal');
-        editModal.querySelector('#newTitle').value = title;
-        editModal.querySelector('#newGenre').value = genre;
-        editModal.querySelector('#newRating').value = rating;
-        document.getElementById('addMovieContainer').style.display = 'none';
-        editModal.style.display = 'block';
-        document.getElementById('submitEdit').addEventListener('click', async function () {
-            const newTitle = editModal.querySelector('#newTitle').value;
-            const newGenre = editModal.querySelector('#newGenre').value;
-            const newRating = editModal.querySelector('#newRating').value;
-            if (newTitle || newRating || newGenre) {
-                try {
-                    await fetch(`http://localhost:3000/movies/${movieId}`, {
-                        method: 'PUT',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify({ title: newTitle, rating: newRating, genre: newGenre }),
-                    });
-                    movieCard.querySelector('.movieTitle').textContent = newTitle;
-                    movieCard.querySelector('.movieRating').innerHTML = `${newRating}  <i class="fa-solid fa-star fa-2xl" style="color: #004C5B;"></i>`;
-                    movieCard.querySelector('.movieGenre').textContent = newGenre;
-                } catch (error) {
-                    console.error(error);
-                }
-            }
-            editModal.style.display = 'none';
-            document.getElementById('addMovieContainer').style.display = 'block';
-        });
-        document.getElementById('cancelEdit').addEventListener('click', function () {
-            editModal.style.display = 'none';
-            document.getElementById('addMovieContainer').style.display = 'block';
-        });
-    }
-    // Function used to delete a movie from the server and remove it from the DOM.
-    // Function deletes a movie when the "Delete" function is clicked and confirms the delete with a prompt that also updates the movie count.
-    async function deleteMovie(movieCard) {
-        const movieId = parseInt(movieCard.id);
-        const shouldDelete = confirm('Are you sure you want to delete this movie?');
-        if (shouldDelete) {
-            try {
-                await fetch(`http://localhost:3000/movies/${movieId}`, {
-                    method: 'DELETE',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(),
-                });
-                movieCard.remove();
-                movieCount -= 1;
-                updateMovieCount();
-            } catch (error) {
-                console.error(error);
-            }
-        }
-    }
-    // Initial fetch and setup to load and display exisating movies.
-    fetchMovies();
-    document.getElementById('addMovieButton').addEventListener('click', async function (e) {
-        e.preventDefault();
-        try {
-            const response = await fetch('http://localhost:3000/movies', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(addMovie()),
-            });
-            const data = await response.json();
-            const movieCard = `
-                    <div id="${data.id}" class="movieCard">
-                        <h2 class="movieTitle">${data.title}</h2>
-                        <p class="movieGenre">${data.genre}</p>
-                        <p class="movieRating">${data.rating} <i class="fa-solid fa-star fa-2xl" style="color: #004C5B;"></i></p>
-                        <button class="editButton">Edit</button>
-                        <button class="deleteButton">Delete</button>
-                    </div>`;
-            document.getElementById('movies').insertAdjacentHTML('beforeend', movieCard);
-            titleArr.push(data.title);
-            ratingArr.push(data.rating);
-            movieCount += 1;
-            updateMovieCount();
-            resetAddMovieForm();
-        } catch (error) {
-            console.error(error);
-        }
-    });
-    document.getElementById('movies').addEventListener('click', function (e) {
-        const target = e.target;
-        if (target.classList.contains('editButton')) {
-            e.preventDefault();
-            displayEditModal(target.closest('.movieCard'));
-        } else if (target.classList.contains('deleteButton')) {
-            e.preventDefault();
-            deleteMovie(target.closest('.movieCard'));
-        }
-    });
-});
